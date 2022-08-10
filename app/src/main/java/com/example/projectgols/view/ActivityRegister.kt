f162e1a0dbcda3dc912259a5d9451b3c720a10d8
riandwisusanto <riandwisusanto03@gmail.com>
2022-08-10 19:29:50 +0700

    fix register

--- app/src/main/java/com/example/projectgols/view/ActivityRegister.kt
+++ app/src/main/java/com/example/projectgols/view/ActivityRegister.kt
@@ -1,12 +1,15 @@
 package com.example.projectgols.view
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import com.example.projectgols.R
+import com.example.projectgols.model.User
+import com.example.projectgols.view.customer.ActivityUtama
+import com.google.firebase.database.FirebaseDatabase
 
 class ActivityRegister : AppCompatActivity() {
     lateinit var namaRegister: EditText
@@ -17,6 +20,7 @@ class ActivityRegister : AppCompatActivity() {
     lateinit var telpRegister: EditText
     lateinit var btnRegister: Button
     lateinit var textLogin: TextView
+    lateinit var SP: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +34,81 @@ class ActivityRegister : AppCompatActivity() {
         telpRegister = findViewById(R.id.telpRegister)
         btnRegister = findViewById(R.id.btnRegister)
         textLogin = findViewById(R.id.textLogin)
+        SP = getSharedPreferences("User", Context.MODE_PRIVATE)
 
         textLogin.setOnClickListener {
             finish()
         }
+
+        btnRegister.setOnClickListener {
+            if (validate()){
+                val ref = FirebaseDatabase.getInstance().getReference("user")
+                val id  = ref.push().key.toString()
+                ref.child(id).setValue(
+                    User(
+                        id,
+                        namaRegister.text.toString(),
+                        emailRegister.text.toString(),
+                        passwordRegister.text.toString(),
+                        alamatRegister.text.toString(),
+                        telpRegister.text.toString(),
+                        "Customer"
+                    )
+                ).addOnSuccessListener {
+                    val editor = SP.edit()
+                    editor.putString("id_user", id)
+                    editor.putString("nama", namaRegister.text.toString())
+                    editor.putString("email", emailRegister.text.toString())
+                    editor.putString("password", passwordRegister.text.toString())
+                    editor.putString("alamat", alamatRegister.text.toString())
+                    editor.putString("telp", telpRegister.text.toString())
+                    editor.putString("level", "Customer")
+                    editor.apply()
+
+                    Toast.makeText(this, "Berhasil register", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this,
+                        ActivityUtama::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun validate(): Boolean {
+        if(namaRegister.text.toString() == "") {
+            Toast.makeText(this, "Nama masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(emailRegister.text.toString() == "") {
+            Toast.makeText(this, "Email masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(passwordRegister.text.toString() == "") {
+            Toast.makeText(this, "Password masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(konfirmasiRegister.text.toString() == "") {
+            Toast.makeText(this, "Konfirmasi masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(alamatRegister.text.toString() == "") {
+            Toast.makeText(this, "Alamat masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(telpRegister.text.toString() == "") {
+            Toast.makeText(this, "Nomor Telepon masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(passwordRegister.text.toString() != konfirmasiRegister.text.toString()) {
+            Toast.makeText(this, "Konfirmasi password tidak sama", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun register(){
+
     }
 }
